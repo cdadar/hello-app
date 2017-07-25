738a9f5d3afc145b869ce0adde9c1a7597d33115
jiarenlu <gunbanmi@gmail.com>
2017-07-25 21:49:14 +0800

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :password_resets,     only: [:new, :create, :edit, :update]
 
-  resources :microposts
+  resources :microposts,          only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'users#index'
